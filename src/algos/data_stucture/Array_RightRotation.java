5ff8a2b5f2f18802a735fa145f3d5d7a89863d3e
Doszhan Baibatyrov <doszhan@live.com>
2018-05-29 14:14:28 +0600

    array-leftrotation

--- src/algos/data_stucture/Array_RightRotation.java
+++ src/algos/data_stucture/Array_RightRotation.java
@@ -1,31 +1,25 @@
 package algos.data_stucture;
+
 import algos.BaseClase;
 
-public class Array_LeftRotation extends BaseClase {
-    public Array_LeftRotation() {
+public class Array_RightRotation extends BaseClase {
+    public Array_RightRotation() {
         int[] arr = {1, 2, 3, 4, 5};
 
         print(arr, "before: ");
-        arr = rotate(arr, 2);
+        rotateToRight(arr, 2);
         print(arr, "after: ");
     }
 
-    public int[] rotate(int[] arr, int k) {
-        int[] rotated = new int[arr.length];
-        for (int i = 0; i < arr.length; i++)
-            rotated[(i + arr.length - k) % arr.length] = arr[i];
-        return rotated;
-    }
 
-
-    static void rotate2(int[] arr, int k) {
+    static void rotateToRight(int[] arr, int k) {
         k %= arr.length;
         int a = arr.length - k;
 
-        reverse(arr, 0, arr.length - 1); //  here is change
+
         reverse(arr, 0, a - 1);
         reverse(arr, a, arr.length - 1);
-
+        reverse(arr, 0, arr.length - 1); // here is chnage
 
         for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
@@ -40,6 +34,4 @@ public class Array_LeftRotation extends BaseClase {
             end--;
         }
     }
-
-
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
 
-import algos.data_stucture.Array_LeftRotation;
 
 public class Main {
-
     public static void main(String[] args) {
-        Array_LeftRotation test = new Array_LeftRotation();
+
     }
 }
